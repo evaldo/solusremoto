1050b9b0c173ef81b3df633a63079abd3d5afae5
Evaldo de Oliveira da Silva <evaldo.oliveira@gmail.com>
2020-08-13 07:55:30 -0300

    Alteração do arquivo de config por causa do novo IP publico. Porém, se manteve o mesmo.

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 	//'Location: http://sistemas.vilaverde.saude.ws/integracao/index.php'
 	class Config 
 	{
-		static $dbHost = '187.16.185.242';
+		static $dbHost = '186.248.180.100';
 		static $dbPort = '5430';
-		static $webServer = 'Location: http://localhost/integracao/index.php';		
+		static $webServer = 'Location: http://infgervilaverde.saude.ws/infger/integracao/index.php';		
 		static $webLogin = 'Location: http://localhost/integracao/login.php';
 		static $webLogout = 'Location: http://localhost/integracao/logout.php';
 		static $destroy="N";

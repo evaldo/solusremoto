7e1727d3d790a7839b53dcba9857c0d831621afe
Evaldo de Oliveira da Silva <evaldo.oliveira@gmail.com>
2020-12-18 00:10:48 -0300

    Manipulação de consulta e insert no SQLServer pelo PHP. Sucesso total!!

--- testesSQLServer/testemssql.php
+++ testesSQLServer/testemssql.php
@@ -1,16 +1,13 @@
 <?php
-// 52.67.44.208
+	
+	error_reporting(0);
+	
+	$connection_string = 'DRIVER={SQL Server};SERVER=191.235.100.131,2071;DATABASE=Sd_ClinVilaVerde_Teste';
 
-$connection_string = 'DRIVER={SQL Server};SERVER=191.235.100.131,2071;DATABASE=Sd_ClinVilaVerde';
+	$user = 'vilaverde';
+	$pass = '&tH5#@06bJT';
 
-$user = 'vilaverde';
-$pass = '&tH5#@06bJT';
-
-$conn = odbc_connect( $connection_string, $user, $pass );
-
-//$serverName = "191.235.100.131,2071";
-//$connectionInfo = array( "Database"=>"Sd_ClinVilaVerde", "UID"=>"vilaverde", "PWD"=>"&tH5#@06bJT");
-//$conn = sqlsrv_connect( $serverName, $connectionInfo);
+	$conn = odbc_connect( $connection_string, $user, $pass );
 
 	if( $conn ) {
 		 echo "Connection established.<br />";
@@ -25,7 +22,33 @@ $conn = odbc_connect( $connection_string, $user, $pass );
 			echo "".$row->PAC_REG." - ".$row->PAC_NOME." </br>" ;					
 		}
 		
-		echo(json_encode($output));
+		$result = odbc_exec($conn, 'select getdate() as data_atual ');
+	
+		if(!$result) {
+			exit("Erro ao consultar Data no Smart");
+		}
+		
+		$row=odbc_fetch_object($result);
+		
+		$data_1 = $row->data_atual;
+		$data_1 = explode("/", $data_1);
+		list($dia, $mes, $ano) = $data_1;
+		$data_1 = "$dia/$mes/$ano";	
+		
+		$data_2 = $row->data_atual;		
+		
+		$nm_obj = "teste bom!!!";
+		$dt_exec_obj = $data_1;
+		$dt_exec_obj_2 = $data_2;
+		
+		 $sql = "insert into dbo.log_exec_obj (nm_obj, dt_exec_obj, dt_exec_obj_segundo) VALUES ('".$nm_obj."', '".$dt_exec_obj."', '".$dt_exec_obj_2."')";
+		 echo $sql;
+		if (!odbc_exec($conn, $sql)) {
+			print("SQL statement failed with error:\n");
+			print(odbc_error($conn).": ".odbc_errormsg($conn)."\n");
+		} else {
+			print("1 rows inserted.\n");
+		}
 		
 		unset($conn); 
 		unset($result);

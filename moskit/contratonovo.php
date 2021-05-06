49924c58a614e56d9e1ad307125576aacb4b018f
Evaldo de Oliveira da Silva <evaldo.oliveira@gmail.com>
2021-05-06 19:24:01 -0300

    Carga do moskit

--- moskit/contratonovo.php
+++ moskit/contratonovo.php
@@ -10,7 +10,7 @@ $curl = curl_init();
 //https://moskit.stoplight.io/docs/api-v1/spec-api-v1.yaml/paths/~1activities/get
 
 curl_setopt_array($curl, [
-  CURLOPT_URL => "https://api.moskitcrm.com/v1/deals",
+  CURLOPT_URL => "https://api.moskitcrm.com/v2/deals",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
@@ -19,7 +19,7 @@ curl_setopt_array($curl, [
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => [
     "Prefer: code=200",
-    "apikey: 8bdfd523-0bd9-4311-a1c9-4c5b706c6739"
+    "apikey: 3e249198-a0f7-4408-ba5c-dc53a9838677"
   ],
 ]);
 
@@ -34,98 +34,27 @@ if ($err) {
 
 $json_array=json_decode($response,true); 
 
+
 $sql = "delete from integracao.tb_moskit_cnto";
-
 $result = pg_query($pdo, $sql);
-
 if($result){
 	echo "";
 } 
 
-display_array_recursive_parte_1($json_array);
-display_array_recursive_parte_2($json_array);
-
-function display_array_recursive_parte_1($json_rec){
+foreach($json_array as $key => $value) { 
+	if ($value["status"] == 'OPEN') {
+		$sql = "insert into integracao.tb_moskit_cnto (id_moskit_deal, ds_entde_cnto, nm_cnto, ds_status) values (".$value["id"].", '".$value["origin"]."', '".$value["name"]."', '".$value["status"]."')";    
+	} else {
+		$sql = "insert into integracao.tb_moskit_cnto (id_moskit_deal, ds_entde_cnto, nm_cnto, ds_status, dt_fchto) values (".$value["id"].", '".$value["origin"]."', '".$value["name"]."', '".$value["status"]."', '".$value["closeDate"]."')"; 
+	}
 	
-	$id_anterior=0;
-	$ds_entidade_contato='';
-	$nm_contato='';
-	$ds_status='';
-	$dt_fchto=0;
-	$sql='';
-	$sql_value='';
+	//echo $sql;
 	
-	$pdo = database::connect();
+	$result = pg_query($pdo, $sql);
+	if($result){
+		echo "";
+	} 
 	
-	if($json_rec){
-		foreach($json_rec as $key=> $value){
-			if(is_array($value)){								
-				display_array_recursive_parte_1($value);
-				$sql_value='';
-				
-			}else{
-
-				if ($value <> '0' && $key=='id') {
-					$id_anterior=$value;
-					$sql_value.= $id_anterior.',';
-				}	
-				
-				if ($key=='name') {
-					$nm_contato = $value;
-					$sql_value.= "'".$nm_contato."',";
-						
-				}					
-				
-				if ($value == 'Deal' && $key=='entity') {
-					$ds_entidade_contato = $value;
-					$sql_value.= "'".$ds_entidade_contato."',";
-				}								
-				
-				if ($key=='status' ) {
-					$ds_status = $value;
-					$sql_value.= "'".$ds_status."'";
-					$sql = "insert into integracao.tb_moskit_cnto (id_moskit_deal, ds_entde_cnto, nm_cnto, ds_status) values (".$sql_value.");";
-				
-					//echo $sql .'<br>';
-				
-					$result = pg_query($pdo, $sql);
-
-					if($result){
-						echo "";
-					}  
-				} 					
-			}	
-		}
-
-	}	
 }
-function display_array_recursive_parte_2($json_rec){
-	
-	$id_anterior=0;	
-	
-	$pdo = database::connect();
-	
-	if($json_rec){
-		foreach($json_rec as $key=> $value){
-			if(is_array($value)){								
-				display_array_recursive_parte_2($value);				
-			}else{
-				if ($value <> '0' && $key=='id') {					
-					$id_anterior=$value;					
-				}	
-				
-				if ($key=='closeDate') {					
-					$dt_fchto = $value;
-					$sql = "update integracao.tb_moskit_cnto set dt_fchto = to_timestamp(".$dt_fchto."/1000) where id_moskit_deal = '".$id_anterior."';";
-					
-					$result = pg_query($pdo, $sql);
 
-					if($result){
-						echo "";
-					}  
-				} 					
-			}	
-		}
-	}	
-}
 ?>
